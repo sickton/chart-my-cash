d347abfbbcd305eadd803debeb4f017cda8165c6
Srivathsa Mantrala <svmantra@ncsu.edu>
2026-01-17 18:52:50 -0500

    structure created

---
+++ chartMyCash/src/main/java/com/expense_tracker/chartmycash/ChartMyCashApplication.java
@@ -0,0 +1,13 @@
+package com.expense_tracker.chartmycash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartMyCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartMyCashApplication.class, args);
+	}
+
+}
