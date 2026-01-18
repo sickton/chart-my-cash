b5beab811fc94efb84431f1c0f456e86f8971981
Srivathsa Mantrala <svmantra@ncsu.edu>
2026-01-17 19:25:29 -0500

    Added user entity and enabled MongoDB doc mapping

---
+++ chartMyCash/src/main/java/com/expense_tracker/chartmycash/backend/model/User.java
@@ -0,0 +1,54 @@
+package com.expense_tracker.chartmycash.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+public class User {
+    @Id
+    private String id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
